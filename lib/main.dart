8defad157f3fe5823a5f0eab50061ad93e1f1228
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2024-07-11 22:28:13 +0530

    [feature] - Counter provider using notifier provider

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notifier_provider/pages/counter/counter_page.dart';
+import 'package:notifier_provider/widgets/custom_button.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'NotifierProvider',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NotifierProvider'),
+      ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(20.0),
+          children: const [
+            CustomButton(
+              title: 'Counter',
+              child: CounterPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
